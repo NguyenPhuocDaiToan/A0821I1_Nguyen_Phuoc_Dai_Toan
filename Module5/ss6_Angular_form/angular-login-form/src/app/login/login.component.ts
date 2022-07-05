1ddc2c701e5e3f6ed394cac39f6fb500bf188a06
DAITOAN <daitoan2000@gmail.com>
2022-07-06 00:11:38 +0700

    Login Form

---
+++ Module5/ss6_Angular_form/angular-login-form/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  result: string;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  login() {
+    if (this.loginForm.status === 'VALID') {
+      this.result = 'Login success';
+    }
+  }
+}
