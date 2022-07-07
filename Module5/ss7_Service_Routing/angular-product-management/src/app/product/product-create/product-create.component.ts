db383f9003ed85890cf3710a427ff606b6393b17
DAITOAN <daitoan2000@gmail.com>
2022-07-07 20:09:06 +0700

    product-management

---
+++ Module5/ss7_Service_Routing/angular-product-management/src/app/product/product-create/product-create.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ProductService} from "../../service/product.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+  createForm: FormGroup;
+
+  constructor(private productService : ProductService, private router : Router) { }
+
+  ngOnInit(): void {
+    this.createForm = new FormGroup({
+      name: new FormControl(''),
+      price: new FormControl(''),
+      description: new FormControl('')
+    });
+  }
+
+  save() {
+    console.log(this.createForm);
+    this.productService.save(this.createForm.value);
+    this.router.navigateByUrl('');
+  }
+}
